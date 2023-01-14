49f521fe285bce15b3642dceb3f48fedc2476da5
Ebrahim Hosseini <sehosseini@me.com>
2023-01-14 17:47:49 +0330

    Initial Commit

---
+++ SwiftUI_Tutorials/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  SwiftUI_Tutorials
+//
+//  Created by Ibrahim Hosseini on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
