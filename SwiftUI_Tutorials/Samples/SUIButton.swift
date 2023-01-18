9caffb08540ee3730a0ededddb0b85eb4f16c8d2
sehosseini@me.com <sehosseini@me.com>
2023-01-18 18:37:23 +0330

    Add SWUIButton

--- SwiftUI_Tutorials/Samples/SUIButton.swift
+++ SwiftUI_Tutorials/Samples/SUIButton.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct SUIButton: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 32) {
+            Button("Tap on me") {
+                print("Tapped on me")
+            }
+            
+            Button {
+                print("Tap on me in different way")
+            } label: {
+                Text("Tap on me in different way")
+                    .fontWeight(.bold)
+                    .foregroundColor(.indigo)
+                    .padding()
+                    .border(.indigo, width: 2)
+            }
+            
+            Button {
+                print("Play")
+            } label: {
+                Label("Play", systemImage: "play.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.pink)
+                    .padding()
+                    .border(.pink, width: 2)
+            }
+            
+            Button {
+                print("pause")
+            } label: {
+                Image("hotspot")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.pink)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(.teal, lineWidth: 4))
+            }
+        }
+        
     }
 }
 
