924f44af42dfd83d083e9df7c564e0fdbce52a33
Ebrahim Hosseini <sehosseini@me.com>
2023-01-18 23:54:52 +0330

    add onChange, pickerStyle action to picker

--- SwiftUI_Tutorials/Samples/SUIButton.swift
+++ SwiftUI_Tutorials/Samples/SUIButton.swift
@@ -8,43 +8,96 @@
 import SwiftUI
 
 struct SUIButton: View {
+
+    @State var colorMe = false
+
+    @State private var selectedColor = Color.gray
+
+    @State private var message = ""
+
     var body: some View {
         VStack(spacing: 32) {
-            Button("Tap on me") {
-                print("Tapped on me")
+            
+            Spacer()
+            Button("Tap on me to change the color") {
+                colorMe.toggle()
             }
+            .fontWeight(.black)
+            .foregroundColor(colorMe ? .gray : .blue)
             
             Button {
-                print("Tap on me in different way")
+                colorMe.toggle()
             } label: {
                 Text("Tap on me in different way")
                     .fontWeight(.bold)
-                    .foregroundColor(.indigo)
+                    .foregroundColor(colorMe ? .gray : .indigo)
                     .padding()
                     .border(.indigo, width: 2)
             }
-            
-            Button {
-                print("Play")
-            } label: {
-                Label("Play", systemImage: "play.fill")
-                    .font(.largeTitle)
-                    .foregroundColor(.pink)
-                    .padding()
-                    .border(.pink, width: 2)
+
+            HStack(spacing: 16) {
+                Button {
+                    colorMe.toggle()
+                } label: {
+                    Label("Play", systemImage: "play.fill")
+                        .font(.largeTitle)
+                        .foregroundColor(colorMe ? .gray : .pink)
+                        .padding()
+                        .border(.pink, width: 2)
+                }
+
+                Button {
+                    colorMe.toggle()
+                } label: {
+                    Image("hotspot")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(colorMe ? .gray: .pink)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(.teal, lineWidth: 4))
+                }
+
+                Button {
+                    colorMe.toggle()
+                } label: {
+                    Label("Pause", systemImage: "pause.fill")
+                        .font(.largeTitle)
+                        .foregroundColor(colorMe ? .gray :  .pink)
+                        .padding()
+                        .border(.pink, width: 2)
+                }
             }
-            
-            Button {
-                print("pause")
-            } label: {
-                Image("hotspot")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 50, height: 50)
-                    .foregroundColor(.pink)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(.teal, lineWidth: 4))
+
+            Text(message)
+            Picker("", selection: $message) {
+                Text("Happy").tag("happy")
+                Text("Sad").tag("sad")
+                Text("Bored").tag("bored")
             }
+            .padding()
+            .pickerStyle(SegmentedPickerStyle())
+            .onChange(of: message) { newValue in
+                switch newValue {
+                case "happy": message = "Be happy and joyous"
+                case "sad": message = "Life can be a struggle at time"
+                case "bored": message = "Look for your purpose"
+                default: break
+                }
+            }
+
+
+            Rectangle()
+                .fill(selectedColor)
+
+            Picker("Favorite color",
+                   selection: $selectedColor) {
+                Text("Red").tag(Color.red)
+                Text("Green").tag(Color.green)
+                Text("Blue").tag(Color.blue)
+                Text("Yellow").tag(Color.yellow)
+            } .pickerStyle(SegmentedPickerStyle())
+
         }
         
     }
