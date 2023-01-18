317ca7953feaf5ecd0441432795fe8873c91217e
sehosseini@me.com <sehosseini@me.com>
2023-01-18 18:17:17 +0330

    add some features for image, add Some shape and add gradient color to it

--- SwiftUI_Tutorials/SUIImage.swift
+++ SwiftUI_Tutorials/SUIImage.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct SUIImage: View {
+    let rainbowColors: [Color] = [.red, .orange, .yellow, .green, .blue, .teal, .purple]
+    
     var body: some View {
-        Image("img")
-            .clipShape(Circle())
-            .overlay {
-                Circle().stroke(.white, lineWidth: 4)
-            }
-            .shadow(radius: 7)
+        VStack (spacing: 16) {
+            
+            Image("img")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 100)
+                .clipShape(Capsule())
+                .overlay {
+                    Capsule().stroke(.white, lineWidth: 4)
+                }
+                .shadow(radius: 7)
+                .opacity(0.8)
+            
+            Image(systemName: "tortoise.fill")
+                .font(.custom("", size: 100))
+                .foregroundColor(.indigo)
+            
+            Capsule()
+                .fill(AngularGradient(gradient: Gradient(colors: rainbowColors),
+                                     center: .center))
+            
+            Circle()
+                .fill(RadialGradient(colors: rainbowColors,
+                                     center: .center,
+                                     startRadius: 0,
+                                     endRadius: 30))
+            
+            Ellipse()
+                .foregroundColor(.pink)
+                
+            Rectangle()
+                .foregroundColor(.cyan)
+            
+            RoundedRectangle(cornerRadius: 30)
+                .foregroundColor(.brown)
+        }
+        .padding()
     }
 }
 
