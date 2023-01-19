33363f6b9fd3815f4739c6daea2a239a872c3c02
Ebrahim Hosseini <sehosseini@me.com>
2023-01-20 01:07:25 +0330

    Slider

--- SwiftUI_Tutorials/Samples/SUIToggleStepperSlider.swift
+++ SwiftUI_Tutorials/Samples/SUIToggleStepperSlider.swift
@@ -12,11 +12,18 @@ struct SUIToggleStepperSlider: View {
     @State private var bluetooth = true
     @State private var hotspot = true
 
-    @State private var newValue = 0
+    @State private var stepperValue = 0
+
+    @State private var sliderValue = 0.0
 
     var body: some View {
-        VStack(spacing: 32) {
-            VStack(spacing: 16) {
+        VStack(alignment: .leading, spacing: 16) {
+            
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Toggles")
+                    .fontWeight(.heavy)
+                    .padding()
+
                 Toggle("Wi-fi", isOn: $wifi)
 
                 Toggle("Bluetooth", isOn: $bluetooth)
@@ -29,20 +36,40 @@ struct SUIToggleStepperSlider: View {
                 Text("Your connections is: \(wifi ? "Wi-Fi, " : "")\(bluetooth ? "Bluetooth, " : "")\(hotspot ? "Hotspot" : "")")
             }
 
-            VStack {
-                Stepper(value: $newValue,
-                        in: 0...100,
-                        step: 10) {
-                    Label {
-                        Text("Number")
-                            .fontWeight(.thin)
-                        Text("\(newValue)")
-                            .bold()
-                    } icon: {
-                        Image(systemName: "digitalcrown.horizontal.arrow.counterclockwise.fill")
-                    }
+            Divider()
+            Text("Spacer")
+                .fontWeight(.heavy)
+                .padding()
+
+            Stepper(value: $stepperValue,
+                    in: 0...100,
+                    step: 10) {
+                Label {
+                    Text("Number")
+                        .fontWeight(.thin)
+                    Text("\(stepperValue)")
+                        .bold()
+                } icon: {
+                    Image(systemName: "digitalcrown.horizontal.arrow.counterclockwise.fill")
                 }
             }
+
+            Divider()
+
+            Text("Slider")
+                .fontWeight(.heavy)
+                .padding()
+            Slider(value: $sliderValue,
+                   in: 100...1000,
+                   step: 50) {
+                Text("Slider")
+            } minimumValueLabel: {
+                Image(systemName: "sun.min")
+            } maximumValueLabel: {
+                Image(systemName: "sun.min.fill")
+            }
+
+
         }
         .padding()
     }
