b93ec44912b67ca60bf0210661c58d4c67aa8a6f
sehosseini@me.com <sehosseini@me.com>
2023-01-21 12:10:44 +0330

    add landmark project to show the list of parks and show details

---
+++ SwiftUI_Tutorials/Samples/Landmarks/View/LandmarkDetail.swift
@@ -0,0 +1,59 @@
+//
+//  LandmarkDetail.swift
+//  SwiftUI_Tutorials
+//
+//  Created by Ibrahim on 1/21/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        
+        VStack {
+            
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            
+            VStack(alignment: .leading) {
+                
+                Text(landmark.name)
+                    .font(.title)
+                
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                
+                Text(landmark.description)
+                
+            }
+            .padding()
+            
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct Landmarks_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
---
+++ SwiftUI_Tutorials/Samples/Landmarks/View/LandmarkRow.swift
@@ -0,0 +1,35 @@
+//
+//  LandmarkRow.swift
+//  SwiftUI_Tutorials
+//
+//  Created by Ibrahim on 1/21/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    let landmark: Landmark
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
