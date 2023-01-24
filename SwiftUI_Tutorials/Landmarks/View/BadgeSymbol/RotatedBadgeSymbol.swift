4e2b75c138b2b06e09d1b0f3be48c8576123df3b
sehosseini@me.com <sehosseini@me.com>
2023-01-24 11:22:59 +0330

    Drow a badge with Path, GeometryReader

---
+++ SwiftUI_Tutorials/Landmarks/View/BadgeSymbol/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUI_Tutorials
+//
+//  Created by Ibrahim on 1/24/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
