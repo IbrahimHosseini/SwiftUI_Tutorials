49e8a79bade4d52abae917465f67fb3e8db05a48
Ebrahim Hosseini <sehosseini@me.com>
2023-01-26 23:54:31 +0330

    add Edit mode in profile and Edit button for editing

--- SwiftUI_Tutorials/Landmarks/View/Profile/ProfileHost.swift
+++ SwiftUI_Tutorials/Landmarks/View/Profile/ProfileHost.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct ProfileHost: View {
 
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject private var modelDate: ModelData
     @State private var draftProfile = Profile.default
 
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummery(profile: draftProfile)
+
+            HStack{
+                Spacer()
+                EditButton()
+            }
+
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummery(profile: modelDate.profile)
+            } else  {
+                Text("Profile Editor")
+            }
         }
         .padding()
     }
@@ -22,5 +34,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
