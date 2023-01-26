12e0bf0e0852a460770f70bf48b31425c1cab3f8
Ebrahim Hosseini <sehosseini@me.com>
2023-01-27 00:15:21 +0330

    define profile editor

--- SwiftUI_Tutorials/Landmarks/View/Profile/ProfileHost.swift
+++ SwiftUI_Tutorials/Landmarks/View/Profile/ProfileHost.swift
@@ -24,7 +24,7 @@ struct ProfileHost: View {
             if editMode?.wrappedValue == .inactive {
                 ProfileSummery(profile: modelDate.profile)
             } else  {
-                Text("Profile Editor")
+                ProfileEditor(profile: $draftProfile)
             }
         }
         .padding()
