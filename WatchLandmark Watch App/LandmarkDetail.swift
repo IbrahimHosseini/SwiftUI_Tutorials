0e3dc835224cf3fdc974e50d1409dfdaca8e2e2c
Ebrahim Hosseini <sehosseini@me.com>
2023-02-02 00:03:00 +0330

    add landmark detail

--- WatchLandmark Watch App/LandmarkDetail.swift
+++ WatchLandmark Watch App/LandmarkDetail.swift
@@ -8,13 +8,62 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+
+    var landmark: Landmark
+
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id})!
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFit()
+
+                Text(landmark.name)
+                    .font(.headline)
+                    .lineLimit(0)
+
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
+                    Text("Favorite")
+                }
+
+                Divider()
+
+                Text(landmark.park)
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+
+                Text(landmark.state)
+                    .font(.caption)
+
+                Divider()
+
+                MapView(coordinate: landmark.locationCoordinate)
+                    .scaledToFit()
+
+            }
+            .padding(16)
+        }
+        .navigationTitle("Landmarks")
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+
     static var previews: some View {
-        LandmarkDetail()
+        let model = ModelData()
+        return Group {
+            LandmarkDetail(landmark: model.landmarks[0])
+                .environmentObject(model)
+                .previewDevice("Apple Watch Series 5 - 40mm")
+
+            LandmarkDetail(landmark: model.landmarks[1])
+                .environmentObject(model)
+                .previewDevice("Apple Watch Series 5 - 44mm")
+        }
     }
 }
