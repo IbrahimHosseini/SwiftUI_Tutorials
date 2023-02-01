e97eef86074f7be58bb650886af7210b2df100e8
Ebrahim Hosseini <sehosseini@me.com>
2023-02-02 00:01:37 +0330

    add notification

--- WatchLandmark Watch App/NotificationView.swift
+++ WatchLandmark Watch App/NotificationView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct NotificationView: View {
+    var title: String?
+    var message: String?
+    var landmark: Landmark?
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if landmark != nil {
+                CircleImage(image: landmark!.image.resizable())
+                    .scaledToFit()
+            }
+
+            Text(title ?? "Unknown Landmark")
+                .font(.headline)
+
+            Divider()
+
+            Text(message ?? "You are within 5 miles of one of your favorite landmarks.")
+                .font(.caption)
+        }
+        .lineLimit(0)
     }
 }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
-        NotificationView()
+        Group {
+            NotificationView()
+            NotificationView(title: "Turtle Rock",
+                             message: "You are within 5 mile of Turtle Rock.",
+                             landmark: ModelData().landmarks[0])
+        }
     }
 }
