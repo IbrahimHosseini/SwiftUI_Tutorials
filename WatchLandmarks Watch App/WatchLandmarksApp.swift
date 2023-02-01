5680d48e79f122a31e2b0b5d6098eb6d56f7a1f1
sehosseini@me.com <sehosseini@me.com>
2023-02-01 12:07:11 +0330

    Dynamic list with search feature

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Ibrahim on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
