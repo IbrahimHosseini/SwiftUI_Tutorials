6e839aa5733e5c4d247f84340e20a8ab7807f747
Ebrahim Hosseini <sehosseini@me.com>
2023-02-02 01:22:42 +0330

    add a specific landmark list view

--- WatchLandmark Watch App/LandmarkList.swift
+++ WatchLandmark Watch App/LandmarkList.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoriteOnly = false
+
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { (!showFavoriteOnly || $0.isFavorite) }
+    }
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+
+            List {
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorite Only")
+                }
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environmentObject(ModelData())
     }
 }
